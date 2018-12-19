1290b0d40dc9a38e79a410ee191135351e9d7143
Nathan Lowe <nathan.lowe@hyland.com>
2018-12-19 14:00:32 -0500

    Refactor world to use slices and be resettable

--- day15/a.go
+++ day15/a.go
@@ -2,9 +2,6 @@ package day15
 
 import (
 	"fmt"
-	"sort"
-
-	"github.com/beefsack/go-astar"
 
 	"github.com/nlowe/aoc2018/util"
 	"github.com/spf13/cobra"
@@ -26,89 +23,16 @@ func a(input *util.ChallengeInput) int {
 
 	turns := 0
 
-war:
 	for w.AtWar() {
-		sort.Slice(w.Units, w.UnitSortFunc)
-
-		for _, u := range w.Units {
-			if u.hp <= 0 {
-				continue
-			}
-
-			var target *Unit
-
-			// Check if we're within striking distance already
-			var openPoints []*Tile
-			for _, other := range u.Enemies(w) {
-				target = u.pickTarget(target, other)
-
-				if target == nil {
-					openPoints = append(openPoints, other.OpenAdjacentTiles(w)...)
-				}
-			}
-
-			if target == nil {
-				var path astar.Pather
-				var cost float64
-
-				// If not, find the shortest path that puts us within striking distance
-				for _, t := range openPoints {
-					// Pick open starting tiles to help the a* out a bit
-					for _, start := range u.OpenAdjacentTiles(w) {
-						// The path is reversed, with the last element being the source tile
-						p, c, found := astar.Path(start, t)
-						if found && (path == nil || c < cost) {
-							path = p[len(p)-1]
-							cost = c
-						}
-					}
-				}
-
-				if path == nil {
-					// This unit is blocked by walls and friendly units
-					continue
-				}
-
-				// Take a step in that direction
-				step := path.(*Tile)
-				w.Layout[u.x][u.y].occupyingUnit = nil
-				step.occupyingUnit = u
-				u.x = step.x
-				u.y = step.y
-
-				// Check again if we're within striking distance
-				for _, other := range u.Enemies(w) {
-					target = u.pickTarget(target, other)
-				}
-			}
-
-			// Do we have someone to attack?
-			if target != nil {
-				target.hp -= u.attack
-
-				// Remove them from the world if they died
-				if target.hp <= 0 {
-					w.Layout[target.x][target.y].occupyingUnit = nil
-
-					if !w.AtWar() {
-						break war
-					}
-				}
-			}
+		if w.Tick() {
+			turns++
 		}
-
-		turns++
 		//fmt.Printf("After %d round(s):\n", turns)
 		//DumpWorld()
 		//fmt.Println()
 	}
 
-	remainingHP := 0
-	for _, u := range w.Units {
-		if u.hp >= 0 {
-			remainingHP += u.hp
-		}
-	}
+	remainingHP, _, _ := w.Survivors()
 
 	//fmt.Printf("After %d round(s) with remaining health %d:\n", turns, remainingHP)
 	//w.Dump()
--- day15/world.go
+++ day15/world.go
@@ -2,6 +2,7 @@ package day15
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/beefsack/go-astar"
@@ -9,10 +10,12 @@ import (
 )
 
 type World struct {
-	Layout map[int]map[int]*Tile
+	Layout [][]*Tile
 	Units  []*Unit
 	W      int
 	H      int
+
+	source []string
 }
 
 const (
@@ -48,10 +51,15 @@ func (t *Tile) PathNeighbors() (result []astar.Pather) {
 		x int
 		y int
 	}{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} {
-		if _, found := t.w.Layout[t.x+delta.x]; found {
-			if neighbor, found := t.w.Layout[t.x+delta.x][t.y+delta.y]; found && neighbor.canOccupy() {
-				result = append(result, neighbor)
-			}
+		dx := t.x + delta.x
+		dy := t.y + delta.y
+
+		if dx < 0 || dy < 0 || dx > len(t.w.Layout[0]) || dy > len(t.w.Layout) {
+			continue
+		}
+
+		if neighbor := t.w.Layout[dx][dy]; neighbor.canOccupy() {
+			result = append(result, neighbor)
 		}
 	}
 
@@ -74,16 +82,31 @@ func (t *Tile) PathEstimatedCost(to astar.Pather) float64 {
 }
 
 func LoadWorld(input *util.ChallengeInput) *World {
-	result := &World{Layout: map[int]map[int]*Tile{}}
+	result := &World{}
 
-	y := 0
 	for line := range input.Lines() {
-		for x, r := range strings.TrimSpace(line) {
-			if _, found := result.Layout[x]; !found {
-				result.Layout[x] = map[int]*Tile{}
-			}
+		result.source = append(result.source, line)
+	}
 
-			tile := &Tile{x: x, y: y, w: result}
+	result.Reset()
+
+	return result
+}
+
+func (w *World) Reset() {
+	w.Layout = [][]*Tile{}
+
+	for x := 0; x < len(w.source[0]); x++ {
+		w.Layout = append(w.Layout, []*Tile{})
+		for y := 0; y < len(w.source); y++ {
+			w.Layout[x] = append(w.Layout[x], nil)
+		}
+	}
+
+	for y, line := range w.source {
+		w.Layout = append(w.Layout, []*Tile{})
+		for x, r := range strings.TrimSpace(line) {
+			tile := &Tile{x: x, y: y, w: w}
 
 			if r == wall {
 				tile.isWall = true
@@ -98,24 +121,20 @@ func LoadWorld(input *util.ChallengeInput) *World {
 					faction: r,
 				}
 
-				result.Units = append(result.Units, unit)
+				w.Units = append(w.Units, unit)
 				tile.occupyingUnit = unit
 			}
 
-			result.Layout[x][y] = tile
-			if x > result.W {
-				result.W = x
+			w.Layout[x][y] = tile
+			if x > w.W {
+				w.W = x
 			}
 		}
 
-		if y > result.H {
-			result.H = y
+		if y > w.H {
+			w.H = y
 		}
-
-		y++
 	}
-
-	return result
 }
 
 func (w *World) Dump() {
@@ -153,4 +172,89 @@ func (w *World) AtWar() bool {
 	}
 
 	return elvesRemaining && goblinsRemaining
+}
+
+func (w *World) Tick() bool {
+	sort.Slice(w.Units, w.UnitSortFunc)
+
+	for _, u := range w.Units {
+		if u.hp <= 0 {
+			continue
+		}
+
+		var target *Unit
+
+		// Check if we're within striking distance already
+		var openPoints []*Tile
+		for _, other := range u.Enemies(w) {
+			target = u.pickTarget(target, other)
+
+			if target == nil {
+				openPoints = append(openPoints, other.OpenAdjacentTiles(w)...)
+			}
+		}
+
+		if target == nil {
+			var path astar.Pather
+			var cost float64
+
+			// If not, find the shortest path that puts us within striking distance
+			for _, t := range openPoints {
+				// Pick open starting tiles to help the a* out a bit
+				for _, start := range u.OpenAdjacentTiles(w) {
+					// The path is reversed, with the last element being the source tile
+					p, c, found := astar.Path(start, t)
+					if found && (path == nil || c < cost) {
+						path = p[len(p)-1]
+						cost = c
+					}
+				}
+			}
+
+			if path == nil {
+				// This unit is blocked by walls and friendly units
+				continue
+			}
+
+			// Take a step in that direction
+			step := path.(*Tile)
+			w.Layout[u.x][u.y].occupyingUnit = nil
+			step.occupyingUnit = u
+			u.x = step.x
+			u.y = step.y
+
+			// Check again if we're within striking distance
+			for _, other := range u.Enemies(w) {
+				target = u.pickTarget(target, other)
+			}
+		}
+
+		// Do we have someone to attack?
+		if target != nil {
+			target.hp -= u.attack
+
+			// Remove them from the world if they died
+			if target.hp <= 0 {
+				w.Layout[target.x][target.y].occupyingUnit = nil
+
+				if !w.AtWar() {
+					return false
+				}
+			}
+		}
+	}
+
+	return true
+}
+
+func (w *World) Survivors() (remainingHP, count int, faction rune) {
+	for _, u := range w.Units {
+		if u.hp >= 0 {
+			count++
+			faction = u.faction
+			remainingHP += u.hp
+		}
+	}
+
+	return
 }
